4dd884e3010195a277e419012cf75ecff5090936
so <ansso8430@gmail.com>
2022-05-29 23:05:33 +0900

    router, css module

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNabigation from './components/layout/MainNavigation';
+
+function App() {
+  return (
+    <div>
+      <MainNabigation />
+      <Routes>
+        <Route path='/' element={ <AllMeetupsPage /> } />
+        <Route path='/new-meetup' element={ <NewMeetupPage /> } />
+        <Route path='/favorites' element={ <FavoritesPage /> } />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
