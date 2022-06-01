e3c9520c8bbacd26e1c8908e59e50f1a1147a65a
so <ansso8430@gmail.com>
2022-06-01 19:07:52 +0900

    useState, useEffect

---
+++ src/App.js
@@ -0,0 +1,145 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  var [funcShow, setFuncShow] = useState(true);
+  var [classShow, setClassShow] = useState(true);
+  return (
+    <div className="container">
+      <h1>Hello word</h1>
+      <input type="button" value="remove func" onClick={()=>setFuncShow(false)} />
+      <input type="button" value="remove class" onClick={()=>setClassShow(false)} />
+      {funcShow ? <FuncComp initNumber={2}></FuncComp> : null}
+      {classShow ? <ClassComp initNumber={2}></ClassComp> : null}
+    </div>
+  );
+}
+
+var funcStyle = 'color:blue';
+var funcId = 0;
+
+function FuncComp(props) {
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+
+  // var dateState = useState((new Date).toString());
+  // var _date = dateState[0];
+  // var setDate = dateState[1];
+
+  const [_date, setDate] = useState((new Date).toString());
+
+  //componentDidMount 일때만
+  useEffect(function () {
+    console.log('%cfunc => useEffect(componentDidMount)' + (++funcId), funcStyle);
+    document.title = number;
+    return function () {
+      console.log('%cfunc => useEffect return(componentWillUnMount)' + (++funcId), funcStyle);
+    }
+  }, []);
+
+  // number 값을 변경할때만
+  useEffect(function () {
+    console.log('%cfunc => useEffect number (componentDidMount & componentDidUpdate)' + (++funcId), funcStyle);
+    document.title = number;
+    return function () {
+      console.log('%cfunc => useEffect number return(componentDidMount & componentDidUpdate)' + (++funcId), funcStyle);
+    }
+  }, [number]);
+
+  // _date 값을 변경할때만
+  useEffect(function () {
+    console.log('%cfunc => useEffect date (componentDidMount & componentDidUpdate)' + (++funcId), funcStyle);
+    document.title = _date;
+    return function () {
+      console.log('%cfunc => useEffect date return(componentDidMount & componentDidUpdate)' + (++funcId), funcStyle);
+    }
+  }, [_date]);
+
+  console.log('%cfunc => render' + (++funcId), funcStyle);
+
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      {/* <p>Number : {props.initNumber}</p> */}
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input
+        type="button"
+        value="random"
+        onClick={ function () {
+          setNumber(Math.random());
+        }
+      } />
+      <input
+        type="button"
+        value="date"
+        onClick={ function () {
+          setDate((new Date).toString());
+        }
+      } />
+    </div>
+  );
+}
+
+var classStyle = 'color:red';
+
+class ClassComp extends React.Component {
+  state = {
+    number: this.props.initNumber,
+    date: (new Date()).toString()
+  };
+
+  componentWillMount(){
+    console.log('%cclass => componentWillMount', classStyle);
+  }
+
+  componentDidMount(){
+    console.log('%cclass => componentDidMount', classStyle);
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('%cclass => shouldComponentUpdate', classStyle);
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    console.log('%cclass => componentWillUpdate', classStyle);
+  }
+
+  componentDidUpdate(nextProps, nextState){
+    console.log('%cclass => componentDidUpdate', classStyle);
+  }
+
+  componentWillUnmount(){
+    console.log('%cclass => componentWillUnmount', classStyle);
+  }
+
+  render() {
+    console.log('%cclass => render', classStyle);
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        {/* <p>Number : {this.props.initNumber}</p> */}
+        <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
+        <input
+          type="button"
+          value="random"
+          onClick={function () {
+            this.setState({ number: Math.random() });
+          }.bind(this)}
+        />
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: (new Date()).toString() });
+          }.bind(this)}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
