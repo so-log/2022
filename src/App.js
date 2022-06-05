7299d0aab35784d905fa69dcd3c2e0f54b227e91
so <ansso8430@gmail.com>
2022-06-05 23:56:07 +0900

    useEffect, useReducer

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    // 사용자를 로그인으로 설정 > loginHandler를 사용하지 않더라도
+    if (storedUserLoggedInInformation === "1") {
+      // state : update
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  // 로그인 버튼을 누르면
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
